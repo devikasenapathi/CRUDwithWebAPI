0fb63d745796dc2a3d218c9601a7eb21c79c4042
devika <senapathidevika@gmail.com>
2024-05-28 12:32:24 +0530

    WEB ABI COMPLEATED

---
+++ CRUDwithWebAPI/BookRep.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
+using CRUDwithWebAPI;
+
+namespace CRUDwithWebAPI12
+{
+    public class BookRep
+    {
+
+        //string ConnectValues;
+
+        BookShop Regiss = new BookShop();
+
+           string ConnectValues = "Data source=DESKTOP-Q9V2K5P\\SQLEXPRESS;Initial Catalog = Web API;User id=sa;password=Anaiyaan@123;";
+           SqlConnection connection;
+
+        public BookRep()
+        {
+            connection = new SqlConnection(ConnectValues);
+        }
+        public void SignUp1(BookShop Regiss)
+        {
+            try
+
+            {
+
+                var InsertSql = $"Insert into  BookShop Values('{Regiss.BookName}','{Regiss.Authour}','{Regiss.ToatalNoPages}','{Regiss.PublishedBy}',{Regiss.ContactNo})";
+                //var ConnectValues = "Data source=DESKTOP-Q9V2K5P\\SQLEXPRESS;Initial Catalog=batch-8;User id=sa;password=Anaiyaan@123;";
+                //SqlConnection connection = new SqlConnection(ConnectValues);
+                connection.Open();
+                connection.Execute(InsertSql);
+                connection.Close();
+
+            }
+
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+
+            {
+                throw;
+            }
+
+        }
+
+        public IEnumerable<BookShop> SignUpUpdate(string b,long a)
+        {
+            try
+            {
+
+
+                
+
+                    var UpdateSql = $" UPdate BookShop set ToatalNoPages= {a} where BookName ='{b}';";
+                   // SqlConnection connection = new SqlConnection(ConnectValues);
+                    connection.Open();
+                    var selectsall = connection.Query<BookShop>(UpdateSql);
+                    connection.Close();
+                    return selectsall;
+                
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+
+        public object SignUpUpdate(string b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookShop> DeleteTable(string BookName)
+
+        {
+            try
+            {
+                var DeleteSql = $"Delete from BookShop where BookName='{BookName}';";
+                SqlConnection connection = new SqlConnection(ConnectValues);
+                connection.Open();
+                var selectsall = connection.Query<BookShop>(DeleteSql);
+                connection.Close();
+                return selectsall;
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public IEnumerable<BookShop> Select()
+
+        {
+            try
+            {
+                var selectquary = $"select * from BookShop ";
+                SqlConnection connection = new SqlConnection(ConnectValues);
+                connection.Open();
+                var selectsall = connection.Query<BookShop>(selectquary);
+                connection.Close();
+                return selectsall;
+
+
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ CRUDwithWebAPI/Controllers/BookshopController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CRUDwithWebAPI12;
+using System.Threading.Tasks;
+using CRUDwithWebAPI;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDwithWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookshopController : ControllerBase
+
+    {
+        BookRep Regis;
+        public BookshopController()
+        {
+            Regis = new BookRep();
+        }
+        // GET: api/<BookshopController>
+        [HttpGet]
+        public IEnumerable<BookShop> Get()
+        {
+            return Regis.Select();
+        }
+
+        // GET api/<BookshopController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BookshopController>
+        [HttpPost]
+        public void Post([FromBody] BookShop Regiss)
+        {
+            Regis.SignUp1(Regiss);
+
+        }
+
+        // PUT api/<BookshopController>/5
+        [HttpPut("{BookName}/{ToatalNoPages}")]
+        public void Put(string BookName,long ToatalNoPages)
+        {
+            Regis.SignUpUpdate(BookName,ToatalNoPages);
+        }
+
+        // DELETE api/<BookshopController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string BookName)
+        {
+            Regis.DeleteTable(BookName);
+        }
+    }
+}
